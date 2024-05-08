e7cd995849ce7b2633945a4d53e757635b949492
astatr1 <astatr1@yandex.ru>
2024-05-08 14:18:51 +0300

    Настроен тест перехода на страницу логина.

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
 
 
 def go_to_login_page(browser):
@@ -8,5 +9,6 @@ def go_to_login_page(browser):
 
 def test_guest_can_do_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
