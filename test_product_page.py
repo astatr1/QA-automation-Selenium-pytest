bd48988dc74bfa1335ad4464c7a268ba9868ff9e
astatr1 <astatr1@yandex.ru>
2024-05-11 16:02:33 +0300

    Добавлено использование тестирования для разных промоакций. Использована маркировка известного бага.

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,14 @@
 from pages.product_page import ProductPage
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = ("http://selenium1py.pythonanywhere.com/catalogue/"
-            "coders-at-work_207/?promo=newYear2019")
+base_url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer'
+
+
+@pytest.mark.parametrize('link', [f"{base_url}{i}" for i in range(10)])
+def test_guest_can_add_product_to_basket(browser, link):
+    if link == f"{base_url}7":
+        pytest.xfail(f"Skipping test for link: {link}")
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
